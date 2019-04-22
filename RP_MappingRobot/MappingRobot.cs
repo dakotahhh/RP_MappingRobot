456a897229ed9f1228ae41f787be147e932f2f1d
Dakotah Reyes <koteyec09@gmail.com>
2019-04-21 19:19:48 -0500

    Robot Class Update
    Removed unused using statement. Added comments.
    Privatized strings and removed ones that were only used in methods from being global variables.
    Renamed Methods.
    Simplified logic in BuildPath so we are using a switch statement instead of a bunch of if else statements. Simplified the logic in DetermineFastestPath so that we only need to assign to finalPath once in each statement.

--- RP_MappingRobot/MappingRobot.cs
+++ RP_MappingRobot/MappingRobot.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RP_MappingRobot
 {
@@ -20,8 +16,7 @@ namespace RP_MappingRobot
         public static void Main(string[] args)
         {
             MappingRobot mappingRobot = new MappingRobot();
-            Console.WriteLine("Welcome to Mapping Robot. Please enter a destination as follows: YYYY-MM-DD; Destination Name; Destination Path ." +
-                "\n For Example: 2017-01-01; Coffee Shop; L2, L5, L5, R5, L2 ");
+            Console.WriteLine("Welcome to Mapping Robot. \n");
             mappingRobot.ReadDestinationInput();
         }
 
@@ -32,11 +27,13 @@ namespace RP_MappingRobot
         /// </summary>
         private void ReadDestinationInput()
         {
+            Console.WriteLine("Please enter a destination as follows: YYYY-MM-DD; Destination Name; Destination Path ." +
+                "\n For Example: 2017-01-01; Coffee Shop; L2, L5, L5, R5, L2 \n");
             string destinationInput = Console.ReadLine();
             string[] destinationValues = destinationInput.Split(';');
             if (destinationValues.Length != 3)
             {
-                Console.WriteLine("Invalid destination. Please check your submission and reenter your destination.");
+                Console.WriteLine("Invalid destination. Please check your submission and reenter your destination. \n");
                 ReadDestinationInput();
             }
             else
@@ -59,21 +56,22 @@ namespace RP_MappingRobot
                               DateTimeStyles.None,
                               out destinationDate))
             {
-                Console.WriteLine("Invalid Destination Date. Please check your submission and reenter your destination.");
+                Console.WriteLine("Invalid Destination Date. Please check your submission and reenter your destination. \n");
                 ReadDestinationInput();
             }
-            string destinationName = destinationValues[1];
+            string destinationName = destinationValues[1].Trim();
             string[] destinationPath = destinationValues[2].Replace(" ", "").Split(',');
             foreach(string s in destinationPath)
             {
                 if(!s.StartsWith("L") && !s.StartsWith("R"))
                 {
-                    Console.WriteLine("Invalid Destination Path. Please check your submission and reenter your destination.");
+                    Console.WriteLine("Invalid Destination Path. Please check your submission and reenter your destination. \n");
                     ReadDestinationInput();
                 }
             }
             Robot robot = new Robot(destinationDate, destinationName, destinationPath);
-            robot.FindFastestPath();
+            Console.WriteLine(robot.BuildPath());
+            Console.ReadLine();
         }
 
     }
--- RP_MappingRobot/Robot.cs
+++ RP_MappingRobot/Robot.cs
@@ -1,115 +1,132 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RP_MappingRobot
 {
+    /// <summary>
+    /// The Robot Class.
+    /// This class creates a robot that contains the method to finding the most optimal route for a destination.
+    /// </summary>
     public class Robot
     {
+        /// <summary>
+        /// None of these properties need to be public as they are only referred to in this class.
+        /// </summary>
+        private DateTime _destinationDate;
+        private string _destinationName;
+        private string[] _destinationPath;
+        private int _west, _north, _east, _south = 0;
 
-        public DateTime DestinationDate;
-        public string DestinationName;
-        public string[] DestinationPath;
-        public char direction;
-        public int west, north, east, south;
-        private string FinalPath;
-
+        /// <summary>
+        /// Creates a Robot with properties passed from the user input.
+        /// </summary>
+        /// <param name="date">the converted date</param>
+        /// <param name="name">the name of the location we are traveling to</param>
+        /// <param name="path">the path taken</param>
         public Robot(DateTime date, string name, string[] path)
         {
-            DestinationDate = date;
-            DestinationName = name;
-            DestinationPath = path;
-            direction = 'W';
+            _destinationDate = date;
+            _destinationName = name;
+            _destinationPath = path;
         }
-        
-        public void FindFastestPath()
+
+        /// <summary>
+        /// We will traverse through each step of the instructed path and determine where to go based off of each step
+        /// We will split up each step so that we can determine how many blocks we have traveled in each direction. 
+        /// We will then call  _DetermineFastestPath() to return the fastest path we can take to our destination.
+        /// </summary>
+        /// <returns>Returns the date, location, and fastest path for the user submitted instructions.</returns>
+        public string BuildPath()
         {
-            char[] stepPath;
-            foreach(string step in DestinationPath)
+            char direction = 'W';
+            foreach(string step in _destinationPath)
             {
-                stepPath = step.ToCharArray();
-                if(direction.Equals('W'))
+                switch(direction)
                 {
-                    if(stepPath[0].Equals('L'))
-                    {
-                        direction = 'S';
-                        south += (int)Char.GetNumericValue(stepPath[1]);
-                    }
-                    else
-                    {
-                        direction = 'N';
-                        north += (int)Char.GetNumericValue(stepPath[1]);
-                    }
-                }
-                else if(direction.Equals('N'))
-                {
-                    if (stepPath[0].Equals('L'))
-                    {
-                        direction = 'W';
-                        west += (int)Char.GetNumericValue(stepPath[1]);
-                    }
-                    else
-                    {
-                        direction = 'E';
-                        east += (int)Char.GetNumericValue(stepPath[1]);
-                    }
-                }
-                else if (direction.Equals('E'))
-                {
-                    if (stepPath[0].Equals('L'))
-                    {
-                        direction = 'N';
-                        north += (int)Char.GetNumericValue(stepPath[1]);
-                    }
-                    else
-                    {
-                        direction = 'S';
-                        south += (int)Char.GetNumericValue(stepPath[1]);
-                    }
-                }
-                else if (direction.Equals('S'))
-                {
-                    if (stepPath[0].Equals('L'))
-                    {
-                        direction = 'E';
-                        east += (int)Char.GetNumericValue(stepPath[1]);
-                    }
-                    else
-                    {
-                        direction = 'W';
-                        west += (int)Char.GetNumericValue(stepPath[1]);
-                    }
+                    case 'W':
+                        if (step.StartsWith("L"))
+                        {
+                            direction = 'S';
+                            _south += Convert.ToInt32(step.Substring(1));
+                        }
+                        else
+                        {
+                            direction = 'N';
+                            _north += Convert.ToInt32(step.Substring(1));
+                        }
+                        break;
+                    case 'N':
+                        if (step.StartsWith("L"))
+                        {
+                            direction = 'W';
+                            _west += Convert.ToInt32(step.Substring(1));
+                        }
+                        else
+                        {
+                            direction = 'E';
+                            _east += Convert.ToInt32(step.Substring(1));
+                        }
+                        break;
+                    case 'E':
+                        if (step.StartsWith("L"))
+                        {
+                            direction = 'N';
+                            _north += Convert.ToInt32(step.Substring(1));
+                        }
+                        else
+                        {
+                            direction = 'S';
+                            _south += Convert.ToInt32(step.Substring(1));
+                        }
+                        break;
+                    case 'S':
+                        if (step.StartsWith("L"))
+                        {
+                            direction = 'E';
+                            _east += Convert.ToInt32(step.Substring(1));
+                        }
+                        else
+                        {
+                            direction = 'W';
+                            _west += Convert.ToInt32(step.Substring(1));
+                        }
+                        break;
+                    default:
+                        break;
                 }
             }
 
-            if(north > south)
+            return _destinationDate.ToString("yyyy-MM-dd") + "; " + _destinationName + "; " + _DetermineFastestPath();
+
+        }
+
+        /// <summary>
+        /// Logic to determine the final path is as follows:
+        /// Essentially North and East are always going to be Right values while South and West are always going to be Left values
+        /// If the amount of steps we have taken North is greater than we have taken South, then we know that going North is the fastest way to get to a location and vice versa
+        /// We will subtract the number of steps we have taken south from the amount of steps we have taken from north to get the shortest amount of steps there and vice versa.
+        /// The same rules apply for East and West.
+        /// If North and South are the same value, we will default to North so long as we can turn left and right.
+        /// </summary>
+        /// <returns>The most optimal route to reach our destination</returns>
+        private string _DetermineFastestPath()
+        {
+            string finalPath = "";
+
+            if (_north >= _south)
+                finalPath += "R" + (_north - _south);
+            else
+                finalPath += "L" + (_south - _north);
+            finalPath += ", ";
+            
+            if (_east >= _west)
             {
-                FinalPath += 'R';
-                FinalPath += (north - south);
+                if(_east != _west)
+                    finalPath += "R" + (_east - _west);
             }
             else
-            {
-                FinalPath += 'L';
-                FinalPath += (south - north);
-                FinalPath += " ";
-            }
-            if(east > west)
-            {
-                FinalPath += 'R';
-                FinalPath += (east - west);
-            }
-            else
-            {
-                FinalPath += 'L';
-                FinalPath += (west - east);
-                FinalPath += " ";
-            }
+                finalPath += "L" + (_west - _east);
 
-            Console.WriteLine(FinalPath);
-            Console.ReadLine();
-
+            return finalPath;
         }
     }
 }
